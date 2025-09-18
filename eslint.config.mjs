e266df4fe4bd8d0db73defead2dcaabdef5ce333
2hanbyeol1 <2hanbyeol1@naver.com>
2025-09-18 17:32:08 +0900

    [CHORE] eslint, prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,16 +10,36 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["prettier", "react", "simple-import-sort"],
+    rules: {
+      "prettier/prettier": ["error"],
+      "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // Packages `react` related packages come first.
+            ["^react", "^@?\\w"],
+            // Internal packages.
+            ["^(@|components)(/.*|$)"],
+            // Side effect imports.
+            ["^\\u0000"],
+            // Parent imports. Put `..` last.
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+            // Other relative imports. Put same-folder imports and `.` last.
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            // Style imports.
+            ["^.+\\.?(css)$"],
+          ],
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;

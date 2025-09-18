e82dcab85b9ac1e78a4354cfb41b2835b7f7e06e
2hanbyeol1 <2hanbyeol1@naver.com>
2025-09-19 00:51:54 +0900

    [CHORE] husky 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">멍로드</div>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-green-700">멍로드</h1>
+    </div>
   );
 }

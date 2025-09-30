a7b4c2ee1a07ea2e0e09308b316dbcee59b72a24
Sangmwi Han <gkstkdgnl12@naver.com>
2025-09-30 16:54:54 +0900

    [REFACTOR] 회원가입 페이지 및 폼 컴포넌트 스타일 수정: 이미지 크기 및 여백 조정, 폰트 크기 변경

--- src/app/(auth)/signup/_components/Form/FormLoginLink.tsx
+++ src/app/(auth)/signup/_components/Form/FormLoginLink.tsx
@@ -5,10 +5,12 @@ import Link from "next/link";
 export function LoginLink() {
   return (
     <div className="flex items-center justify-center gap-1 pt-2 select-none md:pt-4">
-      <span className="text-font-inactive text-sm">이미 회원이신가요?</span>
+      <span className="text-font-inactive text-xs md:text-sm">
+        이미 회원이신가요?
+      </span>
       <Link
         href="/signin"
-        className="text-primary text-sm font-medium underline underline-offset-2 transition-colors hover:opacity-80"
+        className="text-primary text-xs font-medium underline underline-offset-2 transition-colors hover:opacity-80 md:text-sm"
       >
         로그인
       </Link>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,11 +9,11 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       data-slot="input"
       className={cn(
         "file:text-foreground placeholder:text-muted-foreground selection:bg-primary/20",
-        "bg-background h-10 w-full min-w-0 rounded-md",
-        "md:h-12",
+        "bg-background w-full min-w-0 rounded-md",
+        "h-10",
         "px-3 py-1",
-        "text-base md:text-lg",
-        "transition-all duration-300",
+        "text-sm md:text-base",
+        "transition-[border-color, ring-color] duration-300",
         "outline-none",
         "file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium",
         "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",

9fddba8ef3a9d1befece273cf6d91b4dc5eaf011
Sangmwi Han <gkstkdgnl12@naver.com>
2025-09-30 15:52:20 +0900

    [REFACTOR] 로그인 링크 스타일 수정: 비활성화 색상 클래스 변경 및 텍스트 클래스 조정

--- src/app/(auth)/signup/_components/Form/FormLoginLink.tsx
+++ src/app/(auth)/signup/_components/Form/FormLoginLink.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function LoginLink() {
   return (
     <div className="flex items-center justify-center gap-1 pt-2 select-none md:pt-4">
-      <span className="text-sm text-gray-600">이미 회원이신가요?</span>
+      <span className="text-font-inactive text-sm">이미 회원이신가요?</span>
       <Link
         href="/signin"
         className="text-primary text-sm font-medium underline underline-offset-2 transition-colors hover:opacity-80"

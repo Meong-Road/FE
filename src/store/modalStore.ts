d5b0c0e290eec549b39a8f2035846424ea421f47
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-01 14:32:31 +0900

    [FEAT] 모달 스토어 생성
    모달에 사용되는 스토어 생성
    
    related to : #27

--- src/store/modalStore.ts
+++ src/store/modalStore.ts
@@ -1,12 +1,10 @@
 import { create } from "zustand";
 
-type ModalPayload = unknown;
 export type ModalType = "requiredPetInfo" | "addPetInfo" | null;
 
 interface ModalStore {
   isOpen: boolean;
   modalType: ModalType;
-  payload?: ModalPayload;
   openModal: (type: ModalType) => void;
   closeModal: () => void;
 }
@@ -14,7 +12,6 @@ interface ModalStore {
 export const useModalStore = create<ModalStore>((set) => ({
   isOpen: false,
   modalType: null,
-  payload: undefined,
   openModal: (type) => set({ isOpen: true, modalType: type }),
   closeModal: () => set({ isOpen: false, modalType: null }),
 }));

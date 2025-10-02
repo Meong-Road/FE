cbe1dc50660ef5b3252d7c5c458a968d5de1b2e8
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-02 15:13:36 +0900

    [REFACTOR] index.ts 수정
    Form, Modal index.ts 파일 수정
    
    related to : #47

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -2,11 +2,14 @@ import { Form as BaseForm } from "./Form";
 import { Control } from "./FormControl";
 import { Description } from "./FormDescription";
 import { Field } from "./FormField";
+import { ImageUpload } from "./FormImageUpload";
 import { Input } from "./FormInput";
 import { Item } from "./FormItem";
 import { Label } from "./FormLabel";
 import { LoginLink } from "./FormLoginLink";
 import { Message } from "./FormMessage";
+import { Radio } from "./FormRadio";
+import { Select } from "./FormSelect";
 import { SignupLink } from "./FormSignupLink";
 import { SocialButtons } from "./FormSocialButtons";
 import { SubmitButton } from "./FormSubmitButton";
@@ -25,6 +28,9 @@ export const Form = Object.assign(BaseForm, {
   SocialButtons,
   LoginLink,
   SignupLink,
+  Select,
+  ImageUpload,
+  Radio,
 });
 
 export * from "./types";

318b9d712eddf9b10f813afb0b77ce44fda40f08
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-02 15:05:50 +0900

    [REFACTOR] 불필요한 타입 삭제
    모달 구현 방식을 변경하면서 불필요한 타입을 삭제
    
    related to : #47

--- src/store/modalStore.ts
+++ src/store/modalStore.ts
@@ -1,17 +1,14 @@
 import { create } from "zustand";
 
-export type ModalType = "requiredPetInfo" | "addPetInfo" | "editPetInfo" | null;
-
 interface ModalStore {
   isOpen: boolean;
-  modalType: ModalType;
-  openModal: (type?: ModalType) => void;
+  openModal: () => void;
   closeModal: () => void;
 }
 
 export const useModalStore = create<ModalStore>((set) => ({
   isOpen: false,
   modalType: null,
-  openModal: (type) => set({ isOpen: true, modalType: type }),
-  closeModal: () => set({ isOpen: false, modalType: null }),
+  openModal: () => set({ isOpen: true }),
+  closeModal: () => set({ isOpen: false }),
 }));

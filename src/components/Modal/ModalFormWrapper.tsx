75e2aad30c6bf07a59464a20326cdc09f0ffd990
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-02 15:04:47 +0900

    [REMOVE] 불필요한 파일 삭제
    모달 컴포넌트를 구성했던 컴포넌트 중 Form 관련 컴포넌트를 Form 컴포넌트로 변경한 후 삭제
    
    related to : #47

--- src/components/Modal/ModalFormWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function ModalFormWrapper({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="mt-2.5 flex w-full flex-col items-center overflow-y-auto">
-      {children}
-    </div>
-  );
-}

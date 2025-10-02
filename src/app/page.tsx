b2557ce12886c7d7f1178d758c2b538ef95cad26
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-02 15:19:32 +0900

    [FEAT] 모달 확인용 버튼 추가
    모달 확인용 버튼 추가(현재는 확인용, 추후 삭제 및 필요한 곳에 모달 추가 예정)
    
    related to : #47

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Modal from "@/components/Modal/Modal";
+import PetInfoModal from "@/components/Modal/PetInfoModal";
 import { useModalStore } from "@/store/modalStore";
 
 export default function Home() {
@@ -10,7 +11,11 @@ export default function Home() {
     <div className="flex h-screen items-center justify-center">
       <h1 className="text-4xl font-bold text-green-700">멍로드</h1>
       <button onClick={() => openModal()}>모달아 열려라</button>
-      {isOpen && <Modal type="first-login" />}
+      {isOpen && (
+        <Modal hasCloseButton>
+          <PetInfoModal type="edit-pet" />
+        </Modal>
+      )}
     </div>
   );
 }

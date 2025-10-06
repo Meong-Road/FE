33d5925062440beef3fef92a2a385c3302949c77
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-06 16:26:38 +0900

    [FEAT] Form Select props 추가
    Select에서 값을 선택했을 때 어떤 값이 선택됐는지 확인할 수 있도록 value와 onChange 추가
    
    related to : #57

--- src/components/Modal/ModalDateSelect.tsx
+++ src/components/Modal/ModalDateSelect.tsx
@@ -5,28 +5,59 @@ import { Select } from "../Form/FormSelect";
 
 interface ModalDateSelectProps {
   name: string;
+  value?: {
+    year: number;
+    month: number;
+    day: number;
+  };
+  onChange?: (value: { year: number; month: number; day: number }) => void;
 }
 
-export function ModalDateSelect({ name }: ModalDateSelectProps) {
+export function ModalDateSelect({
+  name,
+  value,
+  onChange,
+}: ModalDateSelectProps) {
+  const handleYearChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const year = Number(e.target.value);
+    onChange?.({ year, month: value?.month || 0, day: value?.day || 0 });
+  };
+
+  const handleMonthChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const month = Number(e.target.value);
+    onChange?.({ year: value?.year || 0, month, day: value?.day || 0 });
+  };
+
+  const handleDayChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const day = Number(e.target.value);
+    onChange?.({ year: value?.year || 0, month: value?.month || 0, day });
+  };
+
   return (
     <div className="w-full">
       <div className="flex w-full gap-2.5">
         <Select
-          name={`${name}-year`}
+          name={`${name}.year`}
           placeholder="년"
           options={years}
+          value={value?.year}
+          onChange={handleYearChange}
           className="bg-accent flex-1 rounded-xl px-1 py-2"
         />
         <Select
-          name={`${name}-year`}
+          name={`${name}.month`}
           placeholder="월"
           options={months}
+          value={value?.month}
+          onChange={handleMonthChange}
           className="bg-accent flex-1 rounded-xl px-1 py-2"
         />
         <Select
-          name={`${name}-year`}
+          name={`${name}.day`}
           placeholder="일"
           options={days}
+          value={value?.day}
+          onChange={handleDayChange}
           className="bg-accent flex-1 rounded-xl px-1 py-2"
         />
       </div>

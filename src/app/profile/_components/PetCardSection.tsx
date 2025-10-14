af0dd93dd45b3725d8f80b47cc8e57bc6ad68757
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-14 10:45:22 +0900

    [FEAT] 마이페이지에 반려견 정보 모달 추가
    마이페이지에서 반려견 정보 수정 및 추가 버튼 클릭 시 모달이 뜨도록 하는 코드 추가
    
    related to : #87

--- src/app/profile/_components/PetCardSection.tsx
+++ src/app/profile/_components/PetCardSection.tsx
@@ -1,17 +1,33 @@
+"use client";
+
+import Modal from "@/components/Modal/Modal";
+import PetInfoModal from "@/components/Modal/PetInfoModal";
+import { useModalStore } from "@/store/modalStore";
+
 import { PetAdd } from "./PetAdd";
 import { PetCard } from "./PetCard";
 
 export default function PetCardSection() {
+  const { isOpen, modalType, closeModal } = useModalStore();
   return (
-    <ul className="flex flex-wrap gap-6">
-      <PetCard>
-        <PetCard.Image />
-        <PetCard.Info name="멍멍이" age="나이" gender="성별" type="견종" />
-      </PetCard>
-      <PetAdd>
-        <PetAdd.Image />
-        <PetAdd.Btn />
-      </PetAdd>
-    </ul>
+    <>
+      <ul className="flex flex-wrap gap-6">
+        <PetCard>
+          <PetCard.Image />
+          <PetCard.Info name="멍멍이" age="나이" gender="성별" type="견종" />
+        </PetCard>
+        <PetAdd>
+          <PetAdd.Image />
+          <PetAdd.Btn />
+        </PetAdd>
+      </ul>
+
+      {/* 추가 */}
+      {isOpen && modalType && (
+        <Modal hasCloseButton>
+          <PetInfoModal type={modalType} onClose={closeModal} />
+        </Modal>
+      )}
+    </>
   );
 }

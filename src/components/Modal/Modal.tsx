a97a731a421b1be63681beb1b44e8cf479bb7183
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-15 10:46:42 +0900

    [REFACTOR] 버튼 컴포넌트 삭제 후 기본 button 태그로 변경

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { useModalStore } from "@/store/modalStore";
 
 import CloseButton from "../../assets/icons/delete-icon.svg";
-import Button from "../Button";
 
 interface ModalProps {
   hasCloseButton?: boolean;
@@ -19,13 +18,13 @@ export default function Modal({ hasCloseButton, children }: ModalProps) {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
       <div className="bg-card flex max-h-[80vh] min-w-150 flex-col items-center justify-center gap-3 overflow-hidden rounded-xl px-12 py-12">
         {hasCloseButton && (
-          <Button
+          <button
             className="ml-auto cursor-pointer"
             type="button"
             onClick={closeModal}
           >
             <CloseButton />
-          </Button>
+          </button>
         )}
         {children}
       </div>

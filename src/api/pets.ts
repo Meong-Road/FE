0705d9862ca55aa7a612b0e2af049902dfb6acde
Sangmwi Han <gkstkdgnl12@naver.com>
2025-10-18 03:03:39 +0900

    [REFACTOR] Update pet API to use Response type
    - Replaced PetResponse with a more generic Response type across all pet-related API calls for improved type consistency and maintainability.
    - Ensured all API methods now utilize the updated Response type for better clarity in handling responses.

--- src/api/pets.ts
+++ src/api/pets.ts
@@ -3,8 +3,9 @@ import {
   PetInfoUpdateSchema,
 } from "@/components/Modal/_hooks/usePetInfoForm";
 import { API_ENDPOINTS } from "@/lib/constants/endpoints";
-import { PetResponse, PetType } from "@/lib/types/pets";
+import { PetType } from "@/lib/types/pets";
 
+import { Response } from "./types/common";
 import { customFetch } from "./customFetch";
 
 type PetRequestData = Omit<PetType, "id">;
@@ -28,7 +29,7 @@ function petFormData(
 export const petsApi = {
   // 반려동물 상세 조회
   getPetInfo: async (petId: number) => {
-    const response = await customFetch.get<PetResponse<PetType>>(
+    const response = await customFetch.get<Response<PetType>>(
       `${API_ENDPOINTS.PET}/${petId}`,
     );
     return response.result;
@@ -37,7 +38,7 @@ export const petsApi = {
   // 반려동물 정보 수정
   putPetInfo: async (petId: number, data: PetInfoUpdateSchema) => {
     const formData = petFormData(data);
-    const response = await customFetch.put<PetResponse<PetType>>(
+    const response = await customFetch.put<Response<PetType>>(
       `${API_ENDPOINTS.PET}/${petId}`,
       {
         body: JSON.stringify(formData),
@@ -48,7 +49,7 @@ export const petsApi = {
 
   // 반려동물 정보 삭제
   deletePetInfo: async (petId: number) => {
-    const response = await customFetch.delete<PetResponse<string>>(
+    const response = await customFetch.delete<Response<string>>(
       `${API_ENDPOINTS.PET}/${petId}`,
     );
     return response.result;
@@ -57,7 +58,7 @@ export const petsApi = {
   // 반려동물 등록
   postPetInfo: async (data: PetInfoFormSchema) => {
     const formData = petFormData(data);
-    const response = await customFetch.post<PetResponse<PetType>>(
+    const response = await customFetch.post<Response<PetType>>(
       `${API_ENDPOINTS.PET}`,
       {
         body: JSON.stringify(formData),
@@ -68,7 +69,7 @@ export const petsApi = {
 
   // 특정 유저의 반려동물 목록 조회
   getPetInfoByUserId: async (userId: number) => {
-    const response = await customFetch.get<PetResponse<PetType[]>>(
+    const response = await customFetch.get<Response<PetType[]>>(
       `${API_ENDPOINTS.PET}/user/${userId}`,
     );
     return response.result;
@@ -76,7 +77,7 @@ export const petsApi = {
 
   // 내 반려동물 목록 조회
   getMyPetInfo: async () => {
-    const response = await customFetch.get<PetResponse<PetType[]>>(
+    const response = await customFetch.get<Response<PetType[]>>(
       `${API_ENDPOINTS.PET}/my`,
     );
     return response.result;
@@ -84,7 +85,7 @@ export const petsApi = {
 
   // 타입 별 반려동물 조회(추후 확장성 고려, 현재는 "DOG"밖에 없음)
   getPetInfoByPetType: async (petType: string = "DOG") => {
-    const response = await customFetch.get<PetResponse<PetType[]>>(
+    const response = await customFetch.get<Response<PetType[]>>(
       `${API_ENDPOINTS.PET}/type/${petType}`,
     );
     return response.result;

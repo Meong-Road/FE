2a428ceaaff8a9e6b7d9620361a7faee98e182dd
Sangmwi Han <gkstkdgnl12@naver.com>
2025-10-19 19:44:37 +0900

    [REFACTOR] Update User Query Keys and Signout Logic
    - Refactored USER_QUERY_KEYS to use functions for better consistency and flexibility.
    - Updated useSignout hook to clear user data and remove queries more effectively, enhancing state management during signout.

--- src/hooks/auth/useSignout.ts
+++ src/hooks/auth/useSignout.ts
@@ -2,11 +2,14 @@ import { useQueryClient } from "@tanstack/react-query";
 
 import { tokenStorage } from "@/lib/utils/token";
 
+import { USER_QUERY_KEYS } from "../queries/user/queryKey";
+
 export function useSignout() {
   const queryClient = useQueryClient();
 
   return () => {
     tokenStorage.clear();
-    queryClient.removeQueries({ queryKey: ["user", "my"] });
+    queryClient.setQueryData(USER_QUERY_KEYS.all(), null);
+    queryClient.removeQueries({ queryKey: USER_QUERY_KEYS.my() });
   };
 }

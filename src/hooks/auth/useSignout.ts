6ea4ff58bd013b08b55f20b090003fe6e15ad338
Sangmwi Han <gkstkdgnl12@naver.com>
2025-10-19 20:03:38 +0900

    [REFACTOR] Clean Up useSignout Hook Imports
    - Removed unused import of USER_QUERY_KEYS from the useSignout hook, simplifying the code and improving clarity.

--- src/hooks/auth/useSignout.ts
+++ src/hooks/auth/useSignout.ts
@@ -2,8 +2,6 @@ import { useQueryClient } from "@tanstack/react-query";
 
 import { tokenStorage } from "@/lib/utils/token";
 
-import { USER_QUERY_KEYS } from "../queries/user/queryKey";
-
 export function useSignout() {
   const queryClient = useQueryClient();
 

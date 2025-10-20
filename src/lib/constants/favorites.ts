939b8033b070f76593499246b07a222b28817ced
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-20 14:19:57 +0900

    [FEAT] 찜한 모임 목록 탭 constants로 분리
    related to : #135

---
+++ src/lib/constants/favorites.ts
@@ -0,0 +1,14 @@
+export const FAVORITES_TAB = {
+  REGULAR: {
+    key: "regular",
+    label: "정기 모임",
+    value: "regular",
+  },
+  QUICK: {
+    key: "quick",
+    label: "정기 모임",
+    value: "quick",
+  },
+};
+
+export const FAVORITES_TAB_LIST = [FAVORITES_TAB.REGULAR, FAVORITES_TAB.QUICK];

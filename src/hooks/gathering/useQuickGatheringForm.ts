b6668e8619df3038b2ab4ce22414ef27e447c66a
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-22 18:56:00 +0900

    [REFACTOR] 공통 스키마 분리
    정기 모임과 번개 모임에서 공통으로 사용되는 스키마 분리
    
    related to : #161

--- src/hooks/gathering/useQuickGatheringForm.ts
+++ src/hooks/gathering/useQuickGatheringForm.ts
@@ -2,51 +2,16 @@
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import z from "zod";
+
+import { baseDefaultValues, QuickGatheringFormSchema } from "./schemas";
 
 // TODO : 시간과 분을 입력하지 않았을 때 오류 메시지 출력
-const QuickGatheringFormSchema = z.object({
-  photo: z.instanceof(File).optional(),
-  name: z.string().min(1, "모임 이름을 입력해주세요"),
-  description: z.string().optional(),
-  location: z.string(),
-  dateTime: z.object({
-    hour: z.number().min(1).max(24),
-    minute: z.number().min(1).max(60),
-  }),
-  registrationEnd: z
-    .string()
-    .min(1, "마감 날짜를 선택해주세요")
-    .refine((str) => {
-      const date = new Date(str);
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
-      return date > today;
-    }, "마감 날짜는 오늘 이후로 설정해주세요"),
-  isPetRequired: z.boolean(),
-  capacity: z
-    .string()
-    .min(1, "모집 인원을 입력해주세요")
-    .refine(
-      (str) => !isNaN(parseInt(str, 10)) && parseInt(str, 10) >= 5,
-      "정원은 5명 이상이어야 합니다",
-    ),
-});
-
-export type QuickGatheringFormSchema = z.infer<typeof QuickGatheringFormSchema>;
-
 export function useQuickGatheringForm() {
   return useForm<QuickGatheringFormSchema>({
     resolver: zodResolver(QuickGatheringFormSchema),
     defaultValues: {
-      photo: undefined,
-      name: "",
-      description: "",
-      location: "",
+      ...baseDefaultValues,
       dateTime: { hour: 0, minute: 0 },
-      registrationEnd: "undefined",
-      isPetRequired: true,
-      capacity: "5",
     },
     mode: "onChange",
   });
--- src/hooks/gathering/useRegularGatheringForm.ts
+++ src/hooks/gathering/useRegularGatheringForm.ts
@@ -2,53 +2,15 @@
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import z from "zod";
 
-import { DAY_OF_WEEK } from "@/lib/constants/date";
-
-const RegularGatheringFormSchema = z.object({
-  photo: z.instanceof(File).optional(),
-  name: z.string().min(1, "모임 이름을 입력해주세요"),
-  description: z.string().optional(),
-  location: z.string(),
-  days: z
-    .array(z.enum(DAY_OF_WEEK as [string, ...string[]]))
-    .min(1, "모임 날짜를 하나 이상 선택해주세요"),
-  registrationEnd: z
-    .string()
-    .min(1, "마감 날짜를 선택해주세요")
-    .refine((str) => {
-      const date = new Date(str);
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
-      return date > today;
-    }, "마감 날짜는 오늘 이후로 설정해주세요"),
-  isPetRequired: z.boolean(),
-  capacity: z
-    .string()
-    .min(1, "모집 인원을 입력해주세요")
-    .refine(
-      (str) => !isNaN(parseInt(str, 10)) && parseInt(str, 10) >= 5,
-      "정원은 5명 이상이어야 합니다",
-    ),
-});
-
-export type RegularGatheringFormSchema = z.infer<
-  typeof RegularGatheringFormSchema
->;
+import { baseDefaultValues, RegularGatheringFormSchema } from "./schemas";
 
 export function useRegularGatheringForm() {
   return useForm<RegularGatheringFormSchema>({
     resolver: zodResolver(RegularGatheringFormSchema),
     defaultValues: {
-      photo: undefined,
-      name: "",
-      description: "",
-      location: "",
+      ...baseDefaultValues,
       days: [],
-      registrationEnd: "undefined",
-      isPetRequired: true,
-      capacity: "5",
     },
     mode: "onChange",
   });

e9e402e754db15703799b963f70c1e0779193fa6
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-23 13:36:25 +0900

    [FEAT] 모임 생성 API 및 요청, 응답 타입 추가
    related to : #176

--- src/lib/types/gatherings.ts
+++ src/lib/types/gatherings.ts
@@ -46,3 +46,31 @@ export interface ParticipantsType {
   joinedAt: string; // date
   user: UserType;
 }
+
+// 모임 생성 요청을 위한 공통 필드
+export interface CreateGatheringBase {
+  type: EGatheringType;
+  name: string;
+  description: string;
+  location: string; // TODO: 웅덕님 타입 수정 필요하시면 해주세요
+  capacity: number;
+  image: string | null;
+  isPetRequired: boolean;
+  registrationEnd: string;
+}
+
+// 번개모임 생성 요청
+export interface CreateQuickGatheringReq extends CreateGatheringBase {
+  type: EGatheringType.QUICK;
+  dateTime: string;
+}
+
+// 정기모임 생성 요청
+export interface CreateRegularGatheringReq extends CreateGatheringBase {
+  type: EGatheringType.REGULAR;
+  days: string;
+}
+
+export type CreateGatheringReq =
+  | CreateQuickGatheringReq
+  | CreateRegularGatheringReq;

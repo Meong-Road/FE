1ee1aa2745b2c791e20d26f9323cfce7b23b8529
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-26 21:19:48 +0900

    [REFACTOR] useRouter import 경로 변경
    related to : #192

--- src/hooks/queries/gatherings/usePostGathering.ts
+++ src/hooks/queries/gatherings/usePostGathering.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 

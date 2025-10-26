6e2b0de11c2a2f6d3c46943d8b98424807cc108a
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-26 21:08:48 +0900

    [REFACTOR] useUploadGatheringImage을 useMutation로 추가
    related to : #192

--- src/hooks/queries/imageUpload.ts
+++ src/hooks/queries/imageUpload.ts
@@ -1,10 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 
+import { gatheringApi } from "@/api/gatherings";
 import { petsApi } from "@/api/pets";
 import { ImageUploadReq, ImageUploadRes } from "@/api/types/pets";
 
 export function useUploadPetImage() {
   return useMutation<ImageUploadRes, Error, File>({
     mutationFn: (file) => petsApi.uploadPetImage(file),
+  });
+}
+
+export function useUploadGatheringImage() {
+  return useMutation<ImageUploadRes, Error, File>({
+    mutationFn: (file) => gatheringApi.uploadGatheringImage(file),
   });
 }

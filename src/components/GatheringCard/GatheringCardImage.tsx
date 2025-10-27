b99ad9e56b82d0b8565611044b1ec38f64eb48bc
Sangmwi Han <gkstkdgnl12@naver.com>
2025-10-27 17:06:32 +0900

    [UPDATE] Add canceled gathering state check and update GatheringCardImage component
    - Introduced a new utility function `checkIsCanceledGatheringState` to determine if a gathering is canceled.
    - Updated the `GatheringCardImage` component to display appropriate messages for canceled and closed gatherings.

--- src/components/GatheringCard/GatheringCardImage.tsx
+++ src/components/GatheringCard/GatheringCardImage.tsx
@@ -1,19 +1,25 @@
 import Dog from "@/assets/images/dog.svg";
 import { useGatheringStateContext } from "@/hooks/context/useGatheringStateContext";
-import { checkIsClosedGatheringState } from "@/lib/utils/gathering";
+import {
+  checkIsCanceledGatheringState,
+  checkIsClosedGatheringState,
+} from "@/lib/utils/gathering";
 
 import ImageWithFallback from "../common/ImageWithFallback";
 
 export function GatheringCardImage() {
   const { gathering, state } = useGatheringStateContext();
   const isClosedGathering = checkIsClosedGatheringState(state);
+  const isCanceledGathering = checkIsCanceledGatheringState(state);
 
   return (
     <div className="relative flex size-[188px] items-center justify-center overflow-hidden rounded-3xl">
       {isClosedGathering && (
         <div className="absolute inset-0 z-[5] flex items-center justify-center bg-black/60">
           <span className="text-base font-bold text-white">
-            모집이 마감되었어요
+            {isCanceledGathering
+              ? "모임이 취소되었어요"
+              : "모집이 마감되었어요"}
           </span>
         </div>
       )}

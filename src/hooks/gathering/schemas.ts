269a4a80478925db7425bb570553dcc496c28c1c
ddongguri-bing <ckdalssnsk@naver.com>
2025-10-27 11:49:28 +0900

    [REFACTOR] 모임 폼 스키마의 dateTime을 문자열로 변경
    related to : #192

--- src/hooks/gathering/schemas.ts
+++ src/hooks/gathering/schemas.ts
@@ -7,15 +7,6 @@ export const BaseGatheringFormSchema = z.object({
   name: z.string().min(1, "모임 이름을 입력해주세요"),
   description: z.string().optional(),
   location: z.string(),
-  registrationEnd: z
-    .string()
-    .min(1, "마감 날짜를 선택해주세요")
-    .refine((str) => {
-      const date = new Date(str);
-      const today = new Date();
-      today.setHours(0, 0, 0, 0);
-      return date > today;
-    }, "마감 날짜는 오늘 이후로 설정해주세요"),
   isPetRequired: z.boolean(),
   capacity: z
     .string()
@@ -31,22 +22,28 @@ export const baseDefaultValues = {
   name: "",
   description: "",
   location: "",
-  registrationEnd: "undefined",
   isPetRequired: true,
   capacity: "5",
 };
 
 export const QuickGatheringFormSchema = BaseGatheringFormSchema.extend({
-  dateTime: z.object({
-    hour: z.number().min(1).max(24),
-    minute: z.number().min(1).max(60),
-  }),
+  dateTime: z.string().min(1, "모임 날짜와 시간을 선택해주세요"),
+  registrationEnd: z.string(),
 });
 
 export const RegularGatheringFormSchema = BaseGatheringFormSchema.extend({
   days: z
     .array(z.enum(DAY_OF_WEEK as [string, ...string[]]))
     .min(1, "모임 날짜를 하나 이상 선택해주세요"),
+  registrationEnd: z
+    .string()
+    .min(1, "마감 날짜를 선택해주세요")
+    .refine((str) => {
+      const date = new Date(str);
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      return date > today;
+    }, "마감 날짜는 오늘 이후로 설정해주세요"),
 });
 
 export type QuickGatheringFormSchema = z.infer<typeof QuickGatheringFormSchema>;

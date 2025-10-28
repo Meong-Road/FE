1f97aa64d704d15150c1a9d443a3ed4d8e56905b
Sangmwi Han <gkstkdgnl12@naver.com>
2025-10-28 10:17:01 +0900

    [UPDATE] Enhance PetCardImage component with size prop
    - Added a `size` prop to the PetCardImage component to allow dynamic sizing of the image.
    - Refactored the className assignment using the `cn` utility for cleaner code and improved readability.

--- src/components/PetCard/PetCardImage.tsx
+++ src/components/PetCard/PetCardImage.tsx
@@ -1,23 +1,33 @@
 import DogImg from "@/assets/images/dog.svg";
 import ImageWithFallback from "@/components/common/ImageWithFallback";
+import { cn } from "@/lib/utils";
 
 interface PetCardImageProps {
   src: string;
   alt: string;
   className?: string;
+  size?: number;
 }
 
-export function PetCardImage({ src, alt, className }: PetCardImageProps) {
+export function PetCardImage({
+  src,
+  alt,
+  className,
+  size = 112,
+}: PetCardImageProps) {
   return (
     <div className="mb-4 flex justify-center">
       <div
-        className={`bg-background relative flex size-[112px] items-center justify-center overflow-hidden rounded-full border-1 ${className || ""}`}
+        className={cn(
+          "bg-background relative flex size-[112px] items-center justify-center overflow-hidden rounded-full border-1",
+          className,
+        )}
       >
         <ImageWithFallback
           src={src}
           alt={alt}
           fill
-          sizes="112px"
+          sizes={`${size}px`}
           renderFallback={() => <DogImg className="size-full" />}
         />
       </div>

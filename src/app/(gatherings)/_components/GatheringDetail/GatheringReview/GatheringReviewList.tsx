d4da4423dc69bfd5b4cfbbeba62a93b5741227d5
2hanbyeol1 <2hanbyeol1@naver.com>
2025-10-29 10:19:31 +0900

    [FEAT] 리뷰 페이지네이션 및 오류 수정

---
+++ src/app/(gatherings)/_components/GatheringDetail/GatheringReview/GatheringReviewList.tsx
@@ -0,0 +1,72 @@
+import { useParams } from "next/navigation";
+
+import { EmptyState, ErrorState, LoadingState } from "@/components/common";
+import { Pagination } from "@/components/Pagination";
+import { ReviewCard } from "@/components/ReviewCard";
+import { useGetReviewsByGathering } from "@/hooks/queries/reviews";
+import { useSearchParamsState } from "@/hooks/useSearchParamsState";
+import { cn } from "@/lib/utils";
+
+interface GatheringReviewListProps {
+  className?: string;
+}
+export default function GatheringReviewList({
+  className,
+}: GatheringReviewListProps) {
+  const { id } = useParams();
+  const { page } = useSearchParamsState({
+    page: "0",
+  });
+
+  const { data, isPending, isError } = useGetReviewsByGathering({
+    gatheringId: Number(id),
+    page: Number(page),
+  });
+
+  const reviews = data?.content ?? [];
+
+  if (isPending)
+    return (
+      <LoadingState message="리뷰를 불러오고 있어요..." minHeight="200px" />
+    );
+
+  if (isError || !reviews)
+    return (
+      <ErrorState
+        message="리뷰를 불러오는 중 오류가 발생했어요"
+        minHeight="200px"
+      />
+    );
+
+  if (reviews.length === 0)
+    return <EmptyState message="아직 등록된 리뷰가 없어요" minHeight="200px" />;
+
+  return (
+    <>
+      <ReviewCard
+        className={cn("mb-8 border border-[#ddd] px-12 py-10", className)}
+      >
+        {reviews.map((review, idx) => (
+          <>
+            <div key={`review-${review.id}`} className="flex flex-col gap-y-6">
+              <ReviewCard.Profile
+                user={review.user}
+                score={3}
+                date={review.createdAt}
+              />
+              <ReviewCard.Comment>{review.comment}</ReviewCard.Comment>
+            </div>
+            {idx !== reviews.length - 1 && (
+              <ReviewCard.Divider className="my-6" />
+            )}
+          </>
+        ))}
+      </ReviewCard>
+      <Pagination
+        currentPage={data?.page || 0}
+        totalPages={data?.totalPages || 2}
+        scroll={true}
+      />
+    </>
+  );
+}
--- src/app/(gatherings)/_components/GatheringDetail/GatheringReviewSection.tsx
+++ src/app/(gatherings)/_components/GatheringDetail/GatheringReviewSection.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import GatheringReviewCardList from "./GatheringReview/GatheringReviewCardList";
-import ReviewPagination from "./GatheringReview/ReviewPagination";
+import GatheringReviewList from "./GatheringReview/GatheringReviewList";
 
 export default function GatheringReviewSection() {
   return (
     <section>
       <div className="mb-2 ml-2 text-lg font-semibold">리뷰</div>
-      <GatheringReviewCardList className="px-7 py-7" />
-      <ReviewPagination />
+      <GatheringReviewList className="px-7 py-7" />
     </section>
   );
 }

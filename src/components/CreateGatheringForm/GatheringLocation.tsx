42adb0e8353235c3c59639b7411b4c7145f3e08b
aidenseo <sn14188@gmail.com>
2025-10-29 16:33:59 +0900

    [FEAT] location 폼에 연결
    related to: #223

--- src/components/CreateGatheringForm/GatheringLocation.tsx
+++ src/components/CreateGatheringForm/GatheringLocation.tsx
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { useFormContext } from "react-hook-form";
 
 import MapSearchBar from "@/components/Map/_components/MapSearchBar";
 import MapView from "@/components/Map/_components/MapView";
 import { LocationType } from "@/lib/types/location"; // type 협의 필요
 
+function formatLocation(location: LocationType): string {
+  const parts = location.district.split(" ");
+  const district = parts[1]?.includes("구") ? parts[1] : "기타";
+
+  return JSON.stringify({
+    district,
+    latlng: {
+      lat: location.latlng.lat,
+      lng: location.latlng.lng,
+    },
+  });
+}
+
 export default function GatheringLocation() {
+  const form = useFormContext();
   const [selectedPlace, setSelectedPlace] =
     useState<kakao.maps.services.PlaceType | null>(null);
   const [location, setLocation] = useState<LocationType | null>(null);
 
   useEffect(() => {
-    if (!selectedPlace) return;
-    console.log("selectedPlace:", selectedPlace);
-  }, [selectedPlace]);
+    if (!location) return;
+
+    const formatted = formatLocation(location);
+    form.setValue("location", formatted);
+  }, [location]);
+
+  const currLocation = form.watch("location");
 
   return (
     <section>
@@ -26,6 +45,7 @@ export default function GatheringLocation() {
           <div>
             lat/lng: {location.latlng.lat}, {location.latlng.lng}
           </div>
+          <div>백엔드 payload:{currLocation}</div>
         </>
       )}
     </section>

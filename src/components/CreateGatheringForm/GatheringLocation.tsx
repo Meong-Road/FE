f464b456ecb3cb0be9a1bb047c4a728c47ba9dd4
aidenseo <sn14188@gmail.com>
2025-10-31 01:47:10 +0900

    [FEAT] 모임 상세에서 위치 정보 개선
    related to: #245

--- src/components/CreateGatheringForm/GatheringLocation.tsx
+++ src/components/CreateGatheringForm/GatheringLocation.tsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 
 import MapSearchBar from "@/components/Map/_components/MapSearchBar";
 import MapView from "@/components/Map/_components/MapView";
-import { LocationType } from "@/lib/types/location"; // type 협의 필요 (리뷰에 같은 이름의 타입이 있음)
 
 import { useLocationForm } from "../Map/_hooks/useLocationForm";
 
 export default function GatheringLocation() {
   const [selectedPlace, setSelectedPlace] =
     useState<kakao.maps.services.PlaceType | null>(null);
-  const [location, setLocation] = useState<LocationType | null>(null);
+  const [location, setLocation] =
+    useState<kakao.maps.services.ReverseGeocodePlaceType | null>(null);
 
   useLocationForm(location);
 
--- src/components/Map/_hooks/useLocationForm.ts
+++ src/components/Map/_hooks/useLocationForm.ts
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 
-import { LocationType } from "@/lib/types/location";
-
 import { kakaoMapService } from "../_services/kakaoMapService";
 
-export function useLocationForm(location: LocationType | null) {
+export function useLocationForm(
+  location: kakao.maps.services.ReverseGeocodePlaceType | null,
+) {
   const form = useFormContext();
 
   useEffect(() => {

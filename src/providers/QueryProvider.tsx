aeedcefbf96dae88a1febd073ef6ed09eb18713a
2hanbyeol1 <2hanbyeol1@naver.com>
2025-10-30 19:30:10 +0900

    [FEAT] devtool 추가

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -5,6 +5,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function makeQueryClient() {
   return new QueryClient({
@@ -42,6 +43,9 @@ export default function QueryProvider({
   const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }

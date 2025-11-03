42c0418ef7c5e02a8cd42621e5c1671beb3ab7e4
Sangmwi Han <gkstkdgnl12@naver.com>
2025-11-03 15:27:42 +0900

    .

--- src/app/profile/_components/ProfileHeader.tsx
+++ src/app/profile/_components/ProfileHeader.tsx
@@ -1,4 +1,4 @@
-export function ProfileHeader({ title }: { title: string }) {
+export function ProfileHeader({ title = "마이페이지" }: { title?: string }) {
   return (
     <h2 className="mb-6 text-left text-[24px] leading-none font-semibold select-none sm:text-[32px]">
       {title}

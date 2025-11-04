871f3c42344b056b4dd8429d5192def089d73e32
Sangmwi Han <gkstkdgnl12@naver.com>
2025-11-04 09:59:08 +0900

    [REFACTOR] Adjust layout component for improved structure
    - Removed unnecessary padding from the layout component to enhance visual alignment.
    - Maintained minimum height and flex properties for consistent child element positioning.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import walkImage from "@/assets/images/image.png";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative flex min-h-[70vh] w-full flex-col items-center justify-center gap-6 py-6 lg:flex-row lg:gap-12">
+    <div className="relative flex min-h-[70vh] w-full flex-col items-center justify-center gap-6 lg:flex-row lg:gap-12">
       <Image src={walkImage} alt="image" width={450} height={450} />
       {children}
     </div>
